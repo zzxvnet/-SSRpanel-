7813467afde8266a16cd9af55ede4bca2612bad1
mzfqy <mzfqy@vip.qq.com>
2018-12-21 13:33:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,14 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 echo "请输入 数据库地址 > " ; read mip
-echo "请输入 数据库端口 > " ; read mport
+echo "请输入 数据库端口 > " ; 
+read mport
+read mport
+echo "请输入 数据表名 > " ; read mname
 echo "请输入 数据库用户名 > " ; read mid
 echo "请输入 数据库密码 > " ; read mpass
 echo "请输入 节点ID > " ; read nid
 echo "请输入 节点费率 > " ; read nfee
-echo "API_NODE_ID = '$nid'" >> /root/shadowsocks-py-mu/shadowsocks/config.py
 clear
 echo
 echo "#############################################################"
@@ -30,10 +32,5 @@ cd /root
 git clone https://github.com/mzfqy/shadowsocksr
 chmod 777 * -R
 cd shadowsocksr
-echo "{\"host\":\"127.0.0.1\",\"port\": 3306,\"user\": \"ssrpanel\",\"password": \"pass",\"db\": \"ssrpanel\",\"node_id\": 0,\"transfer_mul\": 1.0,\"ssl_enable\": 0,\"ssl_ca\": \"\",\"ssl_cert\": \"\",\"ssl_key\": \"\"}” >usermysql.json
-mv A /bin
-mv B /bin
-mv C /bin
-mv D /bin
-mv ssx /bin
-crontab cron
+echo "{\"host\":\"$mip\",\"port\": $mport,\"user\": \"$mname\",\"password": \"$mpass",\"db\": \"$mname\",\"node_id\": $nid,\"transfer_mul\": $nfee,\"ssl_enable\": 0,\"ssl_ca\": \"\",\"ssl_cert\": \"\",\"ssl_key\": \"\"}” >usermysql.json
+#crontab cron
