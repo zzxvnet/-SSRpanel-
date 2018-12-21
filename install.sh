308b9c3654a0c02a066f57f793e62216dc5ea3d6
mzfqy <mzfqy@vip.qq.com>
2018-12-21 14:11:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
+clear
+echo
+echo "#############################################################"
+echo "# One click Install ShadowsocksR-Python Manyusers Version    #"
+echo "# Author: mzfqy <mzfqy@qq.com>                       #"
+echo "#############################################################"
 echo "请输入 数据库地址 > " ;read mip
 echo "请输入 数据库端口 > " ;read mport
 echo "请输入 数据表名 > " ;read mname
@@ -8,16 +14,9 @@ echo "请输入 数据库用户名 > " ;read mid
 echo "请输入 数据库密码 > " ;read mpass
 echo "请输入 节点ID > " ;read nid
 echo "请输入 节点费率 > " ;read nfee
-clear
-echo
-echo "#############################################################"
-echo "# One click Install ShadowsocksR-Python Manyusers Version    #"
-echo "# Author: mzfqy <mzfqy@qq.com>                       #"
-echo "#############################################################"
-
-echo
+echo Starting......
 echo 正在设置时区
-cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime -r
+cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime -r >/dev/null 2>&1
 echo 正在关闭防火墙
 systemctl stop firewalld.service >/dev/null 2>&1
 systemctl disable firewalld.service>/dev/null 2>&1
@@ -27,7 +26,7 @@ rpm -ivh ncat-7.60-1.x86_64.rpm >/dev/null 2>&1
 ln -s /usr/bin/ncat /bin/nc
 cd /root
 正在配置ssr
-git clone https://github.com/mzfqy/shadowsocksr
+git clone https://github.com/mzfqy/shadowsocksr >/dev/null 2>&1
 chmod 777 * -R
 cd shadowsocksr
 rm usermysql.json -r
